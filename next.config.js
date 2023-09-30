3463df7ffb452fd1c6ba90c7ae197d26e066e228
raul.souza <raul.souza@terceiro-sky.com.br>
2023-09-30 00:41:45 -0400

    chore: add pwa arch

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const withPWA = require("next-pwa")({
+  dest: "public",
+});
+const nextConfig = {
+  ...withPWA({
+    pwa: {
+      dest: "public",
+      register: true,
+      skipWaiting: true,
+    },
+  }),
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
