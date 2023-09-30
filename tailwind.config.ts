819d0aee8aa58b6ce7264aac4c8bb1e7656cd087
raul.souza <raul.souza@terceiro-sky.com.br>
2023-09-29 22:12:21 -0400

    feat: add globally css and tailwins theme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,18 @@
-import type { Config } from 'tailwindcss'
-
+import type { Config } from "tailwindcss";
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/layout/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/hooks/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    colors: {
+      text: { main: "#555555", disabled: "#7D7D7D", white: "#FFFFFF" },
+      primary: "#8200FA",
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
